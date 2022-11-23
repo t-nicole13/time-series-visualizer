830d595e8ca19490c65e64e15d32a9d3b9bddafb
t-nicole13 <33010018+t-nicole13@users.noreply.github.com>
2022-11-22 23:15:41 -0500

    add line plot

--- times_series_visualizer.py
+++ times_series_visualizer.py
@@ -5,16 +5,20 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates = ['date'] ,index_col = 'date', sep = ',')
 
 # Clean data
-df = None
+df = df[(df.value >= df.value.quantile(0.025)) & (df.value <= df.value.quantile(0.975))]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
+    fig = plt.figure()
+    ax = plt.axes()
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
+    plt.plot(df, c='red')
 
 
 
